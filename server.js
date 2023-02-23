94962c6301d5a21472a2aeb4f0249cca40da7a89
Andeson Glay <andersonglay@hotmail.com>
2023-02-23 12:19:09 -0300

    testecommit

---
+++ server.js
@@ -0,0 +1,9 @@
+const { Console } = require('console');
+const http = require ('http');
+const host = 'http://localhost';
+const port = 3000;
+
+http.createServer((req, res) => {
+    res.end('<h1>Working</h1>')
+
+}).listen(port, () => console.log (`Server is running in ${host}:${port}`));
